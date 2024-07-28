2eb38e3055499ad204adff5140eee5c2f73e19cb
seungsuuu <tmdtn013@naver.com>
2024-07-28 18:03:52 +0900

    [#64] ✨ feat : 토큰 리프레쉬 기능 로그아웃에 적용

--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -1,6 +1,8 @@
 import axios from 'axios';
+import refreshAccessToken from './refreshToken';
 
 const handleLogout = async (navigate) => {
+
   try {
     // 로컬 스토리지에서 액세스 토큰 가져오기
     const accessToken = localStorage.getItem('accessToken');
@@ -24,15 +26,24 @@ const handleLogout = async (navigate) => {
       localStorage.removeItem('userRole');
       localStorage.removeItem('userId')
 
+      alert('로그아웃이 완료 되었습니다.');
+
       // 로그아웃 후 리다이렉트
       navigate('/');
       window.location.reload();
     } else {
-      alert('로그아웃 중 오류가 발생했습니다.')
+      console.log('로그아웃 중 오류가 발생했습니다.');
     }
   } catch (error) {
     console.error('로그아웃 중 오류가 발생했습니다.', error);
-    alert(`${error.response.data.message}` || '로그아웃 중 오류가 발생했습니다.');
+
+    // 에러 상태코드 '401' 이고, 서버의 응답 데이터가 'Expired-Token'인 경우 토큰 리프레쉬
+    if (error.response.status == 401 && error.response.data.data == 'Expired-Token') {
+      await refreshAccessToken();
+      handleLogout(navigate); // 다시 수행하고 있던 함수 재호출
+    } else {
+      alert(`${error.response.data.message}` || '로그아웃 중 오류가 발생했습니다.');
+    }
   }
 };
 
