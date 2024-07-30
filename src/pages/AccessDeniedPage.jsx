8c761167ef72b6b93e672bc7ec966f8d7626934b
serim01 <kkhksl13@gmail.com>
2024-07-30 20:47:06 +0900

    [#95]♻️refactor : 일반 유저가 url로 backoffice 접근시 접근 불가 페이지 나오도록 수정

---
+++ src/pages/AccessDeniedPage.jsx
@@ -0,0 +1,13 @@
+// src/pages/AccessDeniedPage.js
+import React from 'react';
+
+const AccessDeniedPage = () => {
+    return (
+        <div style={{ textAlign: 'center', marginTop: '50px' }}>
+            <h1>접근 불가</h1>
+            <p>이 페이지에 접근할 권한이 없습니다.</p>
+        </div>
+    );
+};
+
+export default AccessDeniedPage;
