60e65049d9dfa3a71762f98d7a7227f467c7c4f9
serim01 <kkhksl13@gmail.com>
2024-08-01 15:59:06 +0900

    [#113]✨feat : admin post List 추가

--- src/pages/Admin/AdminPostListPage.jsx
+++ src/pages/Admin/AdminPostListPage.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import AdminPostList from '../../components/AdminPostList';
 
 function AdminPostListPage() {
     return (
         <div className="admin-post-list-page">
-            
+            <AdminPostList/>
         </div>
     );
 }
